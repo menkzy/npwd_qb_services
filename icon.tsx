3b4cd72589eccb6b4588e705a47344be75eccf04
Menkzy <iammenkz@gmail.com>
2022-08-16 20:50:23 +1000

    Update icon.tsx

--- icon.tsx
+++ icon.tsx
@@ -2,5 +2,5 @@ import React from 'react';
 import BusinessCenterIcon from '@mui/icons-material/BusinessCenter';
 
 export const ServicesIcon = () => {
-  return <BusinessCyarn enterIcon fontSize="large" />;
+  return <BusinessCenterIcon fontSize="large" />;
 };
